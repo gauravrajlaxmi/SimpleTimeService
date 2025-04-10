ff27f7621812919188fb2cd7918a7e197d0c2a16
Gaurav Karemore <95206483+gauravrajlaxmi@users.noreply.github.com>
2025-04-10 12:39:16 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,27 @@
+provider "aws" {
+  region = "us-east-1"
+}
 
+module "vpc" {
+  source = "../modules/network"
+}
+
+module "alb" {
+  source = "../modules/alb"
+  vpc_id         = module.vpc.vpc_id
+  public_subnets = module.vpc.public_subnets
+}
+
+module "ecs" {
+  source = "../modules/ecs"
+
+  vpc_id           = module.vpc.vpc_id
+  private_subnets  = module.vpc.private_subnets
+  cluster_name     = "simple-ecs-cluster"
+  alb_target_group = module.alb.target_group_arn
+  alb_listener     = module.alb.listener_arn
+}
+
+output "alb_dns" {
+  value = module.alb.alb_dns_name
+}
